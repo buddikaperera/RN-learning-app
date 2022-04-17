c306f4e32fa268e5af0f9db65be6c93522ef3036
Buddhika Perera <buddhika.p@informaticsint.com>
2022-04-17 19:37:58 +0530

    flex box settings

--- App.js
+++ App.js
@@ -10,11 +10,18 @@ export default function App() {
 	);
 }
 
+///flex box top left  flex: 1,backgroundColor: '#fff',
+/// center left  flex: 1 /justifyContent: 'center'
+///  bottom left flex: 1 /justifyContent: 'flex-end'
+
+/// top right  flex: 1 //alignItems: 'flex-end',
+///center right   flex: 1 /justifyContent: 'center' alignItems: 'flex-end',
+///bottom right flex: 1 /justifyContent: 'flex-end' alignItems: 'flex-end',
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		//alignItems: 'center',
+		justifyContent: 'flex-end',
 	},
 });
