ce2827945c3c68009fe2d980cf2024bbd225f242
Buddhika Perera <buddhika.p@informaticsint.com>
2022-04-18 19:23:43 +0530

    updating state of the reusable components

--- components/auth/UserInput.js
+++ components/auth/UserInput.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { StyleSheet, TextInput, View } from 'react-native';
-import Text from '@kaloraat/react-native-text';
+import { StyleSheet, TextInput, View } from "react-native";
+import Text from "@kaloraat/react-native-text";
 
 const UserInput = (props) => {
-	const { name } = props;
-	return (
-		<View style={{ marginHorizontal: 25 }}>
-			<Text semi color="#ff2222">
-				{name}
-			</Text>
-			<TextInput
-				style={{
-					borderBottomWidth: 0.5,
-					height: 48,
-					borderBottomColor: '#8e93a1',
-					marginBottom: 30,
-				}}
-				onChangeText={(text) => console.log(text)}
-			/>
-		</View>
-	);
+    const { name, setValue, value } = props;
+    return (
+        <View style={{ marginHorizontal: 25 }}>
+            <Text semi color="#ff2222">
+                {name}
+            </Text>
+            <TextInput
+                style={{
+                    borderBottomWidth: 0.5,
+                    height: 48,
+                    borderBottomColor: "#8e93a1",
+                    marginBottom: 30,
+                }}
+                value={value}
+                onChangeText={(text) => setValue(text)}
+            />
+        </View>
+    );
 };
 
 export default UserInput;
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,20 +1,30 @@
-import { StyleSheet, TextInput, View } from 'react-native';
-import Text from '@kaloraat/react-native-text';
-import React from 'react';
-import UserInput from '../components/auth/UserInput';
+import { StyleSheet, TextInput, View } from "react-native";
+import Text from "@kaloraat/react-native-text";
+import React, { useState } from "react";
+import UserInput from "../components/auth/UserInput";
 
 const SignUp = () => {
-	return (
-		<View>
-			<Text large center>
-				Sign Up
-			</Text>
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
 
-			<UserInput name="Name" />
-			<UserInput name="E-mail" />
-			<UserInput name="Password" />
-		</View>
-	);
+    return (
+        <View style={{ justifyContent: "center" }}>
+            <Text large center>
+                Sign Up
+            </Text>
+
+            <UserInput name="Name" value={name} setValue={setName} />
+            <UserInput name="E-mail" value={email} setValue={setEmail} />
+            <UserInput
+                name="Password"
+                value={password}
+                setValue={setPassword}
+            />
+            <Text>{JSON.stringify({ name, email, password }, null, 6)}</Text>
+        </View>
+    );
 };
 
 export default SignUp;
