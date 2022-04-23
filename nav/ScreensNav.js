0177a50f820f37bdd2e75e7257a16dadca4fb4aa
Buddhika Perera <buddhika.p@informaticsint.com>
2022-04-23 11:32:58 +0530

    refactor code with protected routes sign in and out /home

--- nav/ScreensNav.js
+++ nav/ScreensNav.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet, Text, View } from "react-native";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 import Home from "../screens/Home";
+import { AuthContext } from "../context/auth";
 
 const Stack = createNativeStackNavigator();
 
 export default function ScreensNav() {
+    const [state, setState] = useContext(AuthContext);
+
+    const authenticated = state && state.user !== null && state.token !== "";
+
+    console.log("authenticated", authenticated);
+
     return (
         <Stack.Navigator
-            initialRouteName="SignIn"
+            initialRouteName="Home"
             screenOptions={{ headerShown: false }}
         >
-            <Stack.Screen name="SignIn" component={SignIn} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="Home" component={Home} />
+            {authenticated ? (
+                <Stack.Screen name="Home" component={Home} />
+            ) : (
+                <React.Fragment>
+                    <Stack.Screen name="SignIn" component={SignIn} />
+                    <Stack.Screen name="SignUp" component={SignUp} />
+                </React.Fragment>
+            )}
         </Stack.Navigator>
     );
 }
