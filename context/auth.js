b5097ac02a47931d230be30a29c64e031c2f22b7
Buddhika Perera <buddhika.p@informaticsint.com>
2022-04-27 11:34:20 +0530

    refesh token expired issue address with a intersepter

--- context/auth.js
+++ context/auth.js
@@ -2,16 +2,39 @@ import React, { createContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { API } from "../config";
+import { useNavigation } from "@react-navigation/native";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [state, setState] = useState({ user: null, token: "" });
 
+    const navigation = useNavigation();
+
     let token = state && state.token ? state.token : "";
 
     axios.defaults.baseURL = API;
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    ////handle expire token 401 error
+
+    axios.interceptors.response.use(
+        async function (response) {
+            return response;
+        },
+        async function (error) {
+            let res = error.response;
+
+            if (
+                res.status === 401 &&
+                res.config &&
+                !res.config.__isRetryRequest
+            ) {
+                await AsyncStorage.removeItem("@auth");
+                setState({ user: null, token: "" });
+                navigation.navigate("SignIn");
+            }
+        }
+    );
 
     useEffect(() => {
         const loadFromAsyncStorage = async () => {
