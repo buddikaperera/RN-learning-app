9efc88a8e995adda2a1c2658056e837bb8153c26
Buddhika Perera <buddhika.p@informaticsint.com>
2022-05-09 15:36:49 +0530

    Web Scraping completed

---
+++ links/PreviewCard.js
@@ -0,0 +1,45 @@
+import { View, Image, TouchableOpacity } from "react-native";
+import Text from "@kaloraat/react-native-text";
+import React from "react";
+
+const PreviewCard = ({
+    ogTitle = "Untitled",
+    ogDescription = "No Description provided",
+    ogImage = "https://via.placeholder.com/500x500.png?text=Image",
+}) => {
+    return (
+        <View
+            style={{
+                height: 280,
+                backgroundColor: "#fff",
+                width: "92%",
+                borderRadius: 14,
+                shadowColor: "#171717",
+                shadowOffset: { width: -2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+                marginBottom: 20,
+            }}
+        >
+            <Image
+                style={{
+                    height: "35%",
+                    width: "100%",
+                    borderTopRightRadius: 14,
+                    borderTopLeftRadius: 14,
+                }}
+                source={{ uri: ogImage.url }}
+            />
+            <TouchableOpacity>
+                <View style={{ padding: 5, height: 70 }}>
+                    <Text medium style={{ paddingTop: 5, paddingBottom: 5 }}>
+                        {ogTitle}
+                    </Text>
+                    <Text semi>{ogDescription}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default PreviewCard;
--- screens/Post.js
+++ screens/Post.js
@@ -5,10 +5,15 @@ import { SafeAreaView } from "react-native";
 import Text from "@kaloraat/react-native-text";
 import SubmitButton from "../components/auth/SubmitButton";
 
+import orgs from "@uehreka/open-graph-scraper-react-native";
+import urlRegex from "url-regex";
+import PreviewCard from "../links/PreviewCard";
+
 const Post = () => {
     const [link, setLink] = useState("");
     const [loading, setLoading] = useState(false);
     const [title, setTitle] = useState("");
+    const [urlPreview, setUrlPreview] = useState({});
 
     const handleChange = async (text) => {
         console.log(text);
@@ -16,10 +21,23 @@ const Post = () => {
         try {
             setLoading(true);
             setLink(text);
-            setTimeout(() => {
-                console.log("Link Paste", text);
+
+            if (urlRegex({ strict: false }).test(text)) {
+                orgs({ url: text }, (error, results, response) => {
+                    console.log(results);
+
+                    if (results.success) {
+                        setUrlPreview(results);
+                    }
+                    setLoading(false);
+                });
+            } else {
                 setLoading(false);
-            }, 1000);
+            }
+            // setTimeout(() => {
+            //     console.log("Link Paste", text);
+            //     setLoading(false);
+            // }, 1000);
         } catch (error) {
             console.log(error);
             setLoading(false);
@@ -38,7 +56,9 @@ const Post = () => {
                 </Text>
                 <TextInput
                     value={link}
-                    onChangeText={(text) => handleChange(text)}
+                    onChangeText={(text) =>
+                        handleChange(text, text.trim(), true)
+                    }
                     placeholder="Paste the URL"
                     autoCapitalize="none"
                     autoCorrect={false}
@@ -67,7 +87,18 @@ const Post = () => {
                         borderRadius: 31,
                     }}
                 />
-                <View style={{ paddingTop: 35 }}>
+                {urlPreview.success && (
+                    <View
+                        style={{
+                            marginTop: 30,
+                            alignItems: "center",
+                        }}
+                    >
+                        <PreviewCard {...urlPreview} />
+                    </View>
+                )}
+
+                <View style={{ paddingTop: 8 }}>
                     <SubmitButton
                         title="Submit"
                         handleSubmit={handleSubmit}
@@ -75,9 +106,8 @@ const Post = () => {
                     />
                 </View>
             </ScrollView>
-            <View style={{ flex: 1, justifyContent: "flex-end" }}>
-                <FooterTabs />
-            </View>
+
+            <FooterTabs />
         </SafeAreaView>
     );
 };
